c34b9f399ad2486468ab9915d26857117e081d84
= <odtheamenova@gmail.com>
2023-03-08 16:32:10 -0500

    Creando componentes, estableciendo menú y navegación del sitio.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { DocumentosComponent } from './components/documentos/documentos.component';
+import { FormComponent } from './components/form/form.component';
+import { IncidenciasComponent } from './components/incidencias/incidencias.component';
+import { InformesComponent } from './components/informes/informes.component';
+import { PersonasComponent } from './components/personas/personas.component';
+import { TablaComponent } from './components/tabla/tabla.component';
+import { UbicacionesComponent } from './components/ubicaciones/ubicaciones.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:PersonasComponent},
+  {path:"Personas", component:PersonasComponent},
+  {path:"Usuarios", component:UsuariosComponent},
+  {path:"Clientes", component:ClientesComponent},
+  {path:"Ubicaciones", component:UbicacionesComponent},
+  {path:"Incidencias", component:IncidenciasComponent},
+  {path:"Informes", component:InformesComponent},
+  {path:"Documentos", component:DocumentosComponent},
+  {path:"Form", component:FormComponent},
+  {path:"Tabla", component:TablaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,66 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonasComponent } from './components/personas/personas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './components/menu/menu.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { UbicacionesComponent } from './components/ubicaciones/ubicaciones.component';
+import { IncidenciasComponent } from './components/incidencias/incidencias.component';
+import { InformesComponent } from './components/informes/informes.component';
+import { DocumentosComponent } from './components/documentos/documentos.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { FormComponent } from './components/form/form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TablaComponent } from './components/tabla/tabla.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonasComponent,
+    MenuComponent,
+    UsuariosComponent,
+    UbicacionesComponent,
+    IncidenciasComponent,
+    InformesComponent,
+    DocumentosComponent,
+    ClientesComponent,
+    FormComponent,
+    TablaComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
