752bb982e14d43cb61a9f70146e7a98edbeb2ee0
= <odtheamenova@gmail.com>
2023-05-02 20:48:45 -0500

    Final Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from 'src/app/services/login.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +9,33 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  addressForm = this.fb.group({
-    usr: [null, Validators.required],
-    pass: [null, Validators.required]
+  addressForm = new FormGroup({
+    usr: new FormControl('', Validators.required),
+    pass: new FormControl('', Validators.required)
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(public loginService: LoginService) { }
 
+  usr = "";
+  pass = "";
   onSubmit(): void {
-   // this.usr = this.addressForm.controls["usr"].value;
-  //  this.pass = this.addressForm.controls["pass"].value;
+    this.usr = this.addressForm.controls["usr"].value;
+    this.pass = this.addressForm.controls["pass"].value;
+    if (this.usr === "username" && this.pass === "1234") {
+      Swal.fire(
+        '¡Muy bien!',
+        'Has iniciado sesión correctamente',
+        'success'
+      )
+      localStorage.setItem('login', 'login')
+      this.loginService.login.next("login");
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops..',
+        text: 'Datos de inicio de sesión fallidos',
+        footer: 'Intente con: usuario: username y contraseña: 1234'
+      })
+    }
   }
 }
